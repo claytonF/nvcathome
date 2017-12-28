4cd820f813bbd726f8124c66516580419cf2bc19
cfussel <clayton.f@ri-team.com>
2017-12-28 07:20:21 +0000

    delete dist folder and fix issue with MDL components not initialising after route change

--- src/app/ftn/ftn.component.ts
+++ src/app/ftn/ftn.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { DataService } from "../shared/services/data.service";
 
+declare var componentHandler: any;
+
 @Component({
   selector: "app-ftn",
   templateUrl: "./ftn.component.html",
@@ -11,11 +13,16 @@ export class FtnComponent implements OnInit {
   onlineFlag = navigator.onLine;
   results;
   initialfeelings;
+  
   constructor(private DataService: DataService) {
 
     this.initialfeelings = this.DataService.retrieveData();
     this.initialfeelings = this.initialfeelings.initialfeelings;
   }
+
+  ngAfterViewInit() {
+    componentHandler.upgradeDom();
+}
 
   ngOnInit() {}
 
